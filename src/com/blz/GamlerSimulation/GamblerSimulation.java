7f0f239ed4116fd80a7950605f1f680718767339
Pooja Sree <you@example.com>
2022-03-01 01:12:55 +0530

    Addded:Welcome Message

---
+++ src/com/blz/GamlerSimulation/GamblerSimulation.java
@@ -0,0 +1,11 @@
+package com.blz.GamlerSimulation;
+
+/**
+ * Gambler Simulation Game
+ */
+
+public class GamblerSimulation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambler Simulation Game Project");
+    }
+}
