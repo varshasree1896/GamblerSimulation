746861b5778a7eda2721f5ccf94d31631a2302e3
Pooja Sree <you@example.com>
2022-03-01 01:23:24 +0530

    added:StartWithStakeAndBelt

--- src/com/blz/GamlerSimulation/GamblerSimulation.java
+++ src/com/blz/GamlerSimulation/GamblerSimulation.java
@@ -7,5 +7,8 @@ package com.blz.GamlerSimulation;
 public class GamblerSimulation {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambler Simulation Game Project");
+        int stake = 100; // Start with a stake every day
+        int bet = 1; // Bet for every game
+        System.out.println("Your stack is"+" "+ 100+"$"+" " + "for every day and Bet of"+" "+ 1+"$ for every game");
     }
 }
