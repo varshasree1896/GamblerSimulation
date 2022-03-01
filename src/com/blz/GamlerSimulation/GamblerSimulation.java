95fbcd6ee43bad160c3ca29db17cd6ab88be6679
Pooja Sree <you@example.com>
2022-03-02 00:24:18 +0530

    Added:UC5ForMonth

--- src/com/blz/GamlerSimulation/GamblerSimulation.java
+++ src/com/blz/GamlerSimulation/GamblerSimulation.java
@@ -18,22 +18,29 @@ public class GamblerSimulation {
         int choice;
         int money1 = 0;
         int money2 = 0;
-        int noOfDays;
-        for (noOfDays=0;noOfDays<=20;noOfDays++) {
-            choice = (int) ((Math.random() * 10) % 2); //Generate random number to check win or loose condition
-            switch (choice) {
-                case 0:
-                    System.out.println("Gambler win the bet");
-                    System.out.println(("Amount after winning is = " + (money1 = money1 + (stake + (stake / 2)))));
-                    break;
-                case 1:
-                    System.out.println("Gambler loose the bet");
-                    System.out.println(("Amount after loosing is = " + (money2 = money2 - (stake - (stake / 2)))));
-                    break;
+        int count2 = 0;
+        int count1 = 0;
+        int count = 0;
+        int amount1 = 0;
+        int amount2 = 0;
+
+        for (int months = 1; months <= 2; months++) {
+            for (int noOfDays = 1; noOfDays <= 20; noOfDays++) {
+                choice = (int) ((Math.random() * 10) % 2); //Generate random number to check win or loose condition
+                switch (choice) {
+                    case 0:
+                        System.out.println("Gambler win the bet");
+                        System.out.println(("Amount after winning is = " + (money1 = money1 + (stake + (stake / 2)))));
+                        break;
+                    case 1:
+                        System.out.println("Gambler loose the bet");
+                        System.out.println(("Amount after loosing is = " + (money2 = money2 - (stake - (stake / 2)))));
+                        break;
+                }
             }
+            // After each month winning and loosing days and amount
+            System.out.println("After each month Total winning days = " + " " + (count = count1 - count) + " and Amount is = " + (amount1 = money1 - amount1));
+            System.out.println(("After each month Total loosing days = " + " " + (20 - count) + " and Amount is = " + (amount2 = money2 - amount2)));
         }
-        //Total amount after 20 Days
-        System.out.println(("After 20 days Total winning amount is = " + money1));
-        System.out.println(("After 20 days Total loosing amount is = " + money2));
-        }
+    }
 }
